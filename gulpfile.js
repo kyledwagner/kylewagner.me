955ee8ecdeb07d518642653c8ffd2928cdc6de27
kyledwagner <kylewag@me.com>
2019-11-18 20:02:49 +0700

    Trying to install TailwindCSS the correct way. Running into errors with Gulp building.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+gulp.task('css', function () {
+  const postcss = require('gulp-postcss')
+
+  return gulp.src('src/styles.css')
+    // ...
+    .pipe(postcss([
+      // ...
+      require('tailwindcss'),
+      require('autoprefixer'),
+      // ...
+    ]))
+    // ...
+    .pipe(gulp.dest('build/'))
+})
+
+exports.default = task
